5ad024f9e5be9cff67f61345948d933b38c4f852
Pranav Doshetty <pranav.doshetty@gmail.com>
2021-07-18 15:40:59 +0530

    feat: basic auth token implementation

--- data-exchange/src/main/java/org/dataexchange/vertx/App.java
+++ data-exchange/src/main/java/org/dataexchange/vertx/App.java
@@ -7,6 +7,7 @@ package org.dataexchange.vertx;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonArray;
@@ -15,10 +16,13 @@ import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.SessionHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
 
-public class App 
+public class App
 {
     private JsonArray data;
+    private String Secret = "NPd6Q176avia-6oPbJ_jUITPrNEMTOEfUd7PYxZkSrY";
 
     App() {
         data = new JsonArray();
@@ -28,20 +32,23 @@ public class App
         String id = ctx.request().getParam("id");
         HttpServerResponse response = ctx.response();
         if(id == null)
-            response.setStatusCode(400).end();
-
+            response.setStatusCode(400).end("id Required");
+        boolean found = false;
         for(int i = 0; i < data.size(); i++){
-            if(id.equals(data.getJsonObject(i).getString("id")))
+            if(id.equals(data.getJsonObject(i).getString("id"))) {
+                found = true;
                 ctx.response().end(String.valueOf(data.getJsonObject(i)));
+            }
         }
-        response.setStatusCode(404).end();
+        if(!found)
+            response.setStatusCode(404).end("IoT data with matching id not found");
     }
 
     public void postDataInstanceHandler(RoutingContext ctx){
         JsonObject newData = ctx.getBodyAsJson();
         HttpServerResponse response = ctx.response();
         if(newData == null)
-            response.setStatusCode(400).end();
+            response.setStatusCode(400).end("request body required");
 
         data.add(newData);
         ctx.response().end("Created new data instance with id: "+newData.getString("id")+"\n\n");
@@ -52,11 +59,11 @@ public class App
         String id = ctx.request().getParam("id");
         HttpServerResponse response = ctx.response();
         if(id == null)
-            response.setStatusCode(400).end();
+            response.setStatusCode(400).end("id Required");
 
         JsonObject updateData = ctx.getBodyAsJson();
         if(updateData == null)
-            response.setStatusCode(400).end();
+            response.setStatusCode(400).end("request body required");
 
         data.add(updateData);
         ctx.response().end("Data instance with id: "+id+" updated\n\n");
@@ -66,14 +73,17 @@ public class App
         String id = ctx.request().getParam("id");
         HttpServerResponse response = ctx.response();
         if(id == null)
-            response.setStatusCode(400).end();
-
+            response.setStatusCode(400).end("id Required");
+        boolean found = false;
         for(int i = 0; i < data.size(); i++) {
-            if (id.equals(data.getJsonObject(i).getString("id")))
+            if (id.equals(data.getJsonObject(i).getString("id"))) {
+                found = true;
                 data.remove(data.getJsonObject(i));
-                ctx.response().end("Data instance with id: "+id+" deleted\n\n");
+                ctx.response().end("Data instance with id: " + id + " deleted\n\n");
+            }
         }
-        response.setStatusCode(404).end();
+        if(!found)
+            response.setStatusCode(404).end("IoT data with matching id can't be deleted or does not exist");
     }
 
     public void serverStart(){
@@ -84,6 +94,31 @@ public class App
 
         // BodyHandler added to router to ensure http request body gets parsed... otherwise ctx.request.getBodyAsJson() will always return null
         router.route().handler(BodyHandler.create());
+        // FIND: use of session handler and local session store
+        router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
+        // Basic Auth Token implementation
+        router.route().handler(ctx -> {
+            String AuthToken = ctx.request().getHeader("AUTH_TOKEN");
+            if(AuthToken!=null && AuthToken.equals(Secret))
+                ctx.next();
+            else
+                ctx.response().setStatusCode(401).setStatusMessage("UNAUTHORIZED").end();
+        });
+
+//        router.route("/").handler(routingContext -> {
+//          routingContext.response().putHeader("content-type", "text/html").end(
+//              "<form action=\"/server\" method=\"get\">\n" +
+//              "    <div>\n" +
+//              "        <label for=\"name\">Get IoT data based on id :</label>\n" +
+//              "        <input type=\"text\" id=\"id\" name=\"id\" />\n" +
+//              "    </div>\n" +
+//              "    <div class=\"button\">\n" +
+//              "        <button type=\"submit\">READ</button>\n" +
+//              "    </div>" +
+//              "</form>"
+//          );
+//        });
+
 
         router
                 .get("/server/:id")
