76e3cfbbb83ec5665e952e8d5134331b5dbb6d6d
Pranav Doshetty <pranav.doshetty@gmail.com>
2021-07-18 11:16:11 +0530

    fix: modularity and statuscodes

--- data-exchange/src/main/java/org/dataexchange/vertx/App.java
+++ data-exchange/src/main/java/org/dataexchange/vertx/App.java
@@ -13,20 +13,70 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        JsonArray data = new JsonArray()
-                .add(new JsonObject()
-                    .put("currentLevel", 0.57)
-                    .put("id", "04a15c9960ffda227e9546f3f46e629e1fe4132b")
-                    .put("observationDateTime", "2021-05-02T20:45:00+05:30")
-                    .put("measuredDistance", 9.43)
-                    .put("referenceLevel", 10.0)
-                );
+    private JsonArray data;
+
+    App() {
+        data = new JsonArray();
+    }
+
+    public void getDataInstanceHandler(RoutingContext ctx){
+        String id = ctx.request().getParam("id");
+        HttpServerResponse response = ctx.response();
+        if(id == null)
+            response.setStatusCode(400).end();
+
+        for(int i = 0; i < data.size(); i++){
+            if(id.equals(data.getJsonObject(i).getString("id")))
+                ctx.response().end(String.valueOf(data.getJsonObject(i)));
+        }
+        response.setStatusCode(404).end();
+    }
+
+    public void postDataInstanceHandler(RoutingContext ctx){
+        JsonObject newData = ctx.getBodyAsJson();
+        HttpServerResponse response = ctx.response();
+        if(newData == null)
+            response.setStatusCode(400).end();
+
+        data.add(newData);
+        ctx.response().end("Created new data instance with id: "+newData.getString("id")+"\n\n");
+    }
+
+    // FIXME: performs a post instead of put
+    public void putDataInstanceHandler(RoutingContext ctx){
+        String id = ctx.request().getParam("id");
+        HttpServerResponse response = ctx.response();
+        if(id == null)
+            response.setStatusCode(400).end();
+
+        JsonObject updateData = ctx.getBodyAsJson();
+        if(updateData == null)
+            response.setStatusCode(400).end();
+
+        data.add(updateData);
+        ctx.response().end("Data instance with id: "+id+" updated\n\n");
+    }
+
+    public void deleteDataInstanceHandler(RoutingContext ctx){
+        String id = ctx.request().getParam("id");
+        HttpServerResponse response = ctx.response();
+        if(id == null)
+            response.setStatusCode(400).end();
+
+        for(int i = 0; i < data.size(); i++) {
+            if (id.equals(data.getJsonObject(i).getString("id")))
+                data.remove(data.getJsonObject(i));
+                ctx.response().end("Data instance with id: "+id+" deleted\n\n");
+        }
+        response.setStatusCode(404).end();
+    }
+
+    public void serverStart(){
         Vertx vertx = Vertx.vertx();
         HttpServer httpServer = vertx.createHttpServer();
 
@@ -34,68 +84,43 @@ public class App
 
         // BodyHandler added to router to ensure http request body gets parsed... otherwise ctx.request.getBodyAsJson() will always return null
         router.route().handler(BodyHandler.create());
+
         router
                 .get("/server/:id")
                 .produces("application/json")
-                .handler(ctx -> {
-                    String id = ctx.request().getParam("id");
-                    HttpServerResponse response = ctx.response();
-                    response.setChunked(true);
-                    for(int i = 0; i < data.size(); i++){
-                        if(id.equals(data.getJsonObject(i).getString("id")))
-                            response.write(String.valueOf(data.getJsonObject(i)));
-                        else
-                            response.write("Data instance with id: "+id+" NOT FOUND\n\n");
-                    }
-            ctx.response().end();
-        });
+                .handler(this::getDataInstanceHandler);
 
         router
                 .post("/server")
                 .consumes("*/json")
-                .handler(ctx -> {
-                    JsonObject newData = ctx.getBodyAsJson();
-                    data.add(newData);
-                    System.out.println(data);
-            HttpServerResponse response = ctx.response();
-            response.setChunked(true);
-            response.write("Created new data instance with id: "+newData.getString("id")+"\n\n");
-            ctx.response().end();
-        });
+                .handler(this::postDataInstanceHandler);
 
-        //FIXME: performs a post instead of put
         router
                 .put("/server/:id")
                 .consumes("*/json")
-                .handler(ctx -> {
-                    String id = ctx.request().getParam("id");
-                    JsonObject updateData = ctx.getBodyAsJson();
-                    data.add(updateData);
-                    System.out.println(data);
-            HttpServerResponse response = ctx.response();
-            response.setChunked(true);
-            response.write("Data instance with id: "+id+" updated\n\n");
-            ctx.response().end();
-        });
+                .handler(this::putDataInstanceHandler);
 
-        // FIXME: shows data instance deleted even with no matching data
         router
                 .delete("/server/:id")
-                .handler(ctx -> {
-                    HttpServerResponse response = ctx.response();
-                    response.setChunked(true);
-                    String id = ctx.request().getParam("id");
-                    for(int i = 0; i < data.size(); i++) {
-                        if (id.equals(data.getJsonObject(i).getString("id")))
-                            data.remove(data.getJsonObject(i));
-                    }
-                    response.write("Data instance with id: "+id+" deleted\n\n");
-                    ctx.response().end();
-                    System.out.println(data);
-        });
+                .handler(this::deleteDataInstanceHandler);
 
         httpServer
                 .requestHandler(router)
                 .listen(8881);
+
+    }
+
+    public static void main( String[] args )
+    {
+        App appInstance = new App();
+        appInstance.data
+                .add(new JsonObject()
+                    .put("currentLevel", 0.57)
+                    .put("id", "04a15c9960ffda227e9546f3f46e629e1fe4132b")
+                    .put("observationDateTime", "2021-05-02T20:45:00+05:30")
+                    .put("measuredDistance", 9.43)
+                    .put("referenceLevel", 10.0)
+                );
+        appInstance.serverStart();
     }
 }
